459c81ac904935c69c85127c6361fbc8e9dccd74
Gaev Cenne <gaev@evilparadise.com>
2021-08-09 16:12:13 +0200

    update: layout

--- chapitre_08/pokecontext/src/App.js
+++ chapitre_08/pokecontext/src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter, Switch, Link, Route } from "react-router-dom"
 import Home from "./component/Home"
 import Login from "./component/Login"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 	return (
 		<BrowserRouter>
-			<div>
-				<nav>
+			<div className="container-fluid">
+				<nav className="row">
 					<ul>
 						<li><Link to="/">Home</Link></li>
 						<li><Link to="/login">Login</Link></li>
 					</ul>
 				</nav>
+			</div>
+			<div className="container">
 
 				<Switch>
 					<Route exact path="/" component={Home} />
