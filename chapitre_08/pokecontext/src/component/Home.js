a51df7b8145b9d4c1a86270f8b9952b252ee3314
Gaev Cenne <gaev@evilparadise.com>
2021-08-09 17:30:36 +0200

    update: make home

--- chapitre_08/pokecontext/src/component/Home.js
+++ chapitre_08/pokecontext/src/component/Home.js
@@ -1,8 +1,63 @@
+import { useState, useEffect } from "react";
+
 const Home = props => {
 
-    return (
-        <h1>HOME</h1>
-    )
+	const [pokedex, setPokedex] = useState({});
+	const [pokemonID, setPokemonID] = useState(0)
+
+
+	useEffect(() => {
+		
+		if (pokemonID > 0) {
+
+			const getPokedex = async () => {
+				console.log("On cherche dans l'API")
+				const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
+				const data = await res.json()
+				console.log(data)
+				setPokedex(data)
+			}
+
+			getPokedex()
+		}
+
+	}, [pokemonID])
+
+	const handleClick = data => {
+		setPokemonID(Math.floor(Math.random() * (898)) + 1)
+	}
+
+	console.log()
+	
+	return (
+		<div>
+			<h1>HOME</h1>
+			<button onClick={handleClick} className="btn btn-primary">Get a Pokemon</button>
+
+			{
+				Object.entries(pokedex).length > 0
+				?
+					<div className="card mt-5 w-50">
+						<img src={pokedex.sprites.back_default} alt={pokedex.name} />
+						<div className="card-body">
+							<h5>{pokedex.name}</h5>
+							<p>Height : {pokedex.height}</p>
+							<p>Weight : {pokedex.weight}</p>
+							<p>Types :</p>
+							<ul>
+								{ pokedex.types.map( obj => <li>{obj.type.name}</li> ) }
+							</ul>
+							
+						</div>
+					</div>
+
+				: null
+			}
+			
+			
+		</div>
+					
+	)
 }
 
 export default Home
